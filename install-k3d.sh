4239e67c88cd00384157eba0b918681bafe49548
JulianJessen <julian.jessen@online.de>
2020-11-10 15:14:52 +0100

    Create install-k3d.sh

---
+++ install-k3d.sh
@@ -0,0 +1,19 @@
+use the install script to grab the latest release:
+
+function select-version() {
+  dialog \
+  --backtitle "Select Version" \
+  --menu "Version:" 10 40 4 \
+       "Current Version" "latest" \
+        
+  dialog \
+  --backtitle "Select Version" \
+  --prgbox "wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash" 50 25
+}
+
+#wget: wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+#curl: curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+#use the install script to grab a specific release (via TAG environment variable):
+
+#wget: wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v3.0.0 bash
+#curl: curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | TAG=v3.0.0 bash
